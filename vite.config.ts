d7c8385e1a6774c77a8c4ff957471e441e56e04d
DBatz <d.batzer@web.de>
2026-04-15 20:53:36 +0200

    fix devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import devtoolsJson from "vite-plugin-devtools-json";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), devtoolsJson()],
+  plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
   },
